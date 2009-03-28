3711d5d2fda5301a0882e603940017df6b5e9eb6
textarcana <noah@onemorebug.com>
2009-03-28 15:36:58 -0400

    added route for the home page

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,8 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
 
+  map.root :controller => "home", :action => "index"
+
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
