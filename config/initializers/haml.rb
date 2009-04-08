a9ece44684f6fb9d439878161cdc442927f8d1e4
textarcana <noah@onemorebug.com>
2009-04-08 12:22:45 -0400

    XHTML attributes should be double quoted and stylesheets should be human-readable

---
+++ config/initializers/haml.rb
@@ -0,0 +1,3 @@
+# Make double quotes the default when writing HTML element attributes.
+# See http://rails.learnhub.com/debates/463-haml-is-the-best-template-engine-on-rails
+Haml::Template.options = {:attr_wrapper => "\""}
---
+++ config/initializers/sass.rb
@@ -0,0 +1,2 @@
+# Indent stylesheets in the commonly accepted fashion.
+Sass::Plugin.options[:style] = :expanded
